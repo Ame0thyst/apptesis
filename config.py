dca3d398a8b30f67df5802b73aa8d2b1c81e8fb8
Ame0thyst <kokoo15011209@gmail.com>
2025-12-18 09:29:31 +0700

    🐛fix : User siswa -> tidak bisa mengambil tes ulang

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', '')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme123')
     # Gunakan DATABASE_URL dari .env jika ada, fallback ke default Laragon (root tanpa password)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root@localhost/db_rekomendasi')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
